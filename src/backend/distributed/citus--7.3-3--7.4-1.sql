d045d5bccbb17fd6cb7b85ca99ba8b4d4f15db5a
Onder Kalaci <onder@citusdata.com>
2018-04-27 16:49:50 +0300

    Undo extra new line

--- src/backend/distributed/citus--7.3-3--7.4-1.sql
+++ src/backend/distributed/citus--7.3-3--7.4-1.sql
@@ -3,4 +3,3 @@
 DROP FUNCTION IF EXISTS master_expire_table_cache(regclass);
 DROP FUNCTION IF EXISTS pg_catalog.worker_fetch_regular_table(text, bigint, text[], integer[]);
 DROP FUNCTION IF EXISTS pg_catalog.worker_fetch_foreign_file(text, bigint, text[], integer[]);
-
