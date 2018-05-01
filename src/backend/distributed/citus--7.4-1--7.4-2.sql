b2728306fc2898d39fe5a7a21ec2b649dd065523
Onder Kalaci <onder@citusdata.com>
2018-05-01 17:34:18 +0300

    Fix extra line addition

--- src/backend/distributed/citus--7.4-1--7.4-2.sql
+++ src/backend/distributed/citus--7.4-1--7.4-2.sql
@@ -15,6 +15,7 @@ BEGIN
     SELECT array_agg(object_identity) INTO sequence_names
     FROM pg_event_trigger_dropped_objects()
     WHERE object_type = 'sequence';
+
     FOR v_obj IN SELECT * FROM pg_event_trigger_dropped_objects()
                  WHERE object_type IN ('table', 'foreign table')
     LOOP
